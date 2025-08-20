b19bd771f5ea8b6dc37e3488706818fef4335b46
BaeYoona <jinalks43@gmail.com>
2025-08-20 14:54:38 +0900

    [Add] 앱 종료 시 Alert 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,42 @@
-import { Stack } from "expo-router";
+import { router, Stack } from "expo-router";
+import { useEffect } from "react";
+import { Alert, BackHandler, Platform } from "react-native";
 import { AuthProvider } from "../src/contexts/AuthContext";
 import AuthGuard from "./components/AuthGuard";
 
 export default function RootLayout() {
+  useEffect(() => {
+    // Android에서만 동작
+    if (Platform.OS === 'android') {
+      const backAction = () => {
+        if (!router.canGoBack()) {
+          Alert.alert(
+            "앱 종료",
+            "정말로 앱을 종료하시겠습니까?",
+            [
+              {
+                text: "취소",
+                onPress: () => null,
+                style: "cancel"
+              },
+              {
+                text: "종료",
+                onPress: () => BackHandler.exitApp()
+              }
+            ]
+          );
+          return true; // 기본 뒤로가기 동작을 막음
+        }
+      };
+
+      const backHandler = BackHandler.addEventListener(
+        "hardwareBackPress",
+        backAction
+      );
+
+      return () => backHandler.remove();
+    }
+  }, []);
   return (
     <AuthProvider>
       <AuthGuard>
